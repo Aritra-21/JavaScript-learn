cdbcda08fefd64d63c6506bcf351c1774484af52
Aritra Mondal <109265728+Aritra-21@users.noreply.github.com>
2025-07-20 13:21:05 +0000

    conversion of data type

---
+++ basics/conversion.js
@@ -0,0 +1,27 @@
+let score = "90"
+let numStrCombo = "90abc"
+
+console.log(typeof score);
+console.log(typeof numStrCombo);
+
+// console.log(score);
+let convertNumber = Number(score)
+console.log(typeof convertNumber); 
+
+let conNumStrCombo = Number(numStrCombo)
+console.log(typeof conNumStrCombo);
+console.log(conNumStrCombo);
+
+// 90 = number
+// "90abc" = NaN -> Not a Number
+// true => 1, false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+
+
+
