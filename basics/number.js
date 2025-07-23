b0e9378c4465840f4d760969dcb8602137d91067
Aritra Mondal <109265728+Aritra-21@users.noreply.github.com>
2025-07-23 19:14:17 +0000

    about number

---
+++ basics/number.js
@@ -0,0 +1,27 @@
+const score = 400;
+// console.log(score);
+// // 400
+
+const balance = new Number(100)
+// console.log(balance);
+// // [Number: 100]
+
+// console.log(balance.toString().length);
+// // 3
+// console.log(balance.toFixed(2));
+// // 100.00
+
+// const hundred = 1000000
+// // Default standard
+// console.log(hundred.toLocaleString());
+// // 1,000,000
+
+// // Indian standard
+// console.log(hundred.toLocaleString('en-IN'));
+// // 10,00,000
+
+// const otherNumber = 145.8966
+// console.log(otherNumber.toPrecision(3));
+// // 146
+
+
