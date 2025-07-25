48a5f4997bd5b237797bc2cc95445b998abe49f4
Aritra Mondal <109265728+Aritra-21@users.noreply.github.com>
2025-07-25 13:16:13 +0000

    date objects of js

---
+++ basics/date.js
@@ -0,0 +1,79 @@
+// let myDate = new Date()
+
+// console.log(myDate);
+
+
+// console.log(myDate.toString());
+// Fri Jul 25 2025 04:56:38 GMT+0000 (Coordinated Universal Time)
+
+// let myCreatedDate = new Date(2023, 2, 25, 0, 110);
+
+// console.log(myCreatedDate.toDateString());
+
+// console.log(myCreatedDate.toLocaleDateString());
+
+// console.log(myDate.toISOStringo());
+
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toJSON());
+
+// console.log(myDate.toISOStringo());
+
+// mm-dd-yyyy Format (Indian use this)
+// let myNewDate = new Date("03-27-2025")
+// // console.log(myNewDate.toLocaleString());
+
+// console.log(myNewDate.getTime());
+
+// let myTimeStamp = Date.now()
+// // console.log(myTimeStamp);
+// // 1753423185204 mili second
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+// 2025-07-25T13:01:43.536Z
+
+console.log(newDate.getMonth() + 1);
+// 7
+
+console.log(newDate.getDay());
+
+console.log(`${newDate.getMonth() + 1} and the time is ${newDate.getTime()}`);
+// 7 and the time is 1753448868442
+
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+}));
+
+// Friday
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(myDate.);
+
+// console.log(myDate.t);
+
+// console.log(myDate.t);
+
+// console.log(myDate.t);
+
+// console.log(myDate.t);
+
+// console.log(myDate.t);
+
