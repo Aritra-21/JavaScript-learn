bc52d444ea1656b6881ce3a6f1a4e85bc5123987
Aritra Mondal <109265728+Aritra-21@users.noreply.github.com>
2025-08-26 19:37:44 +0000

    object_02 half completed

---
+++ basics/object_02.js
@@ -0,0 +1,28 @@
+// Creating new blank objects
+
+// singletone obj
+
+// const facebookUser = new Object()
+
+// nonsingletone object
+const facebookUser = {}
+
+facebookUser.id = "123"
+facebookUser.name ="Ram"
+facebookUser.isLoggedIn = false
+
+// console.log(facebookUser);
+
+
+// nested obj
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        firstName: "Aritra",
+        lastName: "Mondal"
+
+    }
+
+}
+
+// console.log(regularUser.fullName.firstName);
