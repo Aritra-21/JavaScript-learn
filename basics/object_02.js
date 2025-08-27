9ee3373da61b070fdcd0952e1a48d6cf5dc8e05a
Aritra Mondal <109265728+Aritra-21@users.noreply.github.com>
2025-08-27 06:01:04 +0000

    object_02 completed

--- basics/object_02.js
+++ basics/object_02.js
@@ -26,3 +26,65 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullName.firstName);
+
+// Combine 2 objects
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3:"a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1, obj2}  
+// ans { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+
+// use object.assign
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// all value wii be combined in blank obj
+// obj1,2,4 are source and {}is target 
+
+
+// console.log(obj3);
+
+// mostly use spread operator
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+console.log(obj3);
+
+
+// when we get data from DB it is come as array of objs
+
+const user = [
+    {
+        id:1,
+        email: "a@gmail.com"
+    },
+    {
+        id:1,
+        email: "a@gmail.com"
+    },
+    {
+        id:1,
+        email: "a@gmail.com"
+    },
+    {
+        id:1,
+        email: "a@gmail.com"
+    },
+]
+
+
+
+console.log(user[1]);
+
+console.log(facebookUser);
+console.log(Object.keys(facebookUser));
+console.log(Object.values(facebookUser));
+console.log(Object.entries(facebookUser));
+
+
+// is there 'isLoggedIn' property present in facebookUser obj -> return bool value
+console.log(facebookUser.hasOwnProperty('isLoggedIn')); 
+
+
+
